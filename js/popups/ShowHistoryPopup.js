db2ed1162b1e31dbd72295c5cf1d2eb99a17f7ea
nadine-shi <nadinshi@gmail.com>
2020-07-03 10:33:29 +0300

    Show history popup

---
+++ js/popups/ShowHistoryPopup.js
@@ -0,0 +1,147 @@
+'use strict';
+
+var
+	_ = require('underscore'),
+	ko = require('knockout'),
+	moment = require('moment'),
+
+	TextUtils = require('%PathToCoreWebclientModule%/js/utils/Text.js'),
+
+	Ajax = require('%PathToCoreWebclientModule%/js/Ajax.js'),
+	App = require('%PathToCoreWebclientModule%/js/App.js'),
+	CAbstractPopup = require('%PathToCoreWebclientModule%/js/popups/CAbstractPopup.js'),
+	CPageSwitcherView = require('%PathToCoreWebclientModule%/js/views/CPageSwitcherView.js'),
+	Screens = require('%PathToCoreWebclientModule%/js/Screens.js'),
+
+	Popups = require('%PathToCoreWebclientModule%/js/Popups.js'),
+	ConfirmPopup = require('%PathToCoreWebclientModule%/js/popups/ConfirmPopup.js')
+;
+
+/**
+ * @constructor
+ */
+function CShowHistoryPopup()
+{
+	CAbstractPopup.call(this);
+
+	this.popupHeading = ko.observable('');
+	this.item = null;
+	
+	this.historyItems = ko.observableArray([]);
+	this.isLoading = ko.observable(true);
+	this.isEmptyHistory = ko.computed(function () {
+		return !this.isLoading() && this.historyItems().length === 0;
+	}, this);
+
+	this.iEventsPerPage = 10;
+	this.oPageSwitcher = new CPageSwitcherView(0, this.iEventsPerPage);
+	this.oPageSwitcher.currentPage.subscribe(function () {
+		this.requestEvents();
+	}, this);
+	this.iCurrentPage = 0;
+}
+
+_.extendOwn(CShowHistoryPopup.prototype, CAbstractPopup.prototype);
+
+CShowHistoryPopup.prototype.PopupTemplate = '%ModuleName%_ShowHistoryPopup';
+
+/**
+ * @param {Object} oItem
+ */
+CShowHistoryPopup.prototype.onOpen = function (sPopupHeading, oItem)
+{
+	this.item = oItem;
+	this.popupHeading(sPopupHeading);
+	this.historyItems([]);
+	this.isLoading(true);
+	this.oPageSwitcher.setPage(1, this.iEventsPerPage);
+	this.requestEvents();
+};
+
+CShowHistoryPopup.prototype.requestEvents = function ()
+{
+	Ajax.send('ActivityHistory',
+		'GetList',
+		{
+			'UserId': App.getUserId(),
+			'ResourceType': 'file',
+			'ResourceId': this.item.path() + '/' + this.item.fileName(),
+			'Offset': (this.oPageSwitcher.currentPage() - 1) * this.iEventsPerPage,
+			'Limit': this.iEventsPerPage
+		},
+		this.onGetActivityHistory,
+		this
+	);
+};
+
+CShowHistoryPopup.prototype.onGetActivityHistory = function (oResponse, oRequest)
+{
+	console.log('oResponse.Result', oResponse.Result);
+	oResponse.Result = {
+		Items: [
+			{time: moment().add(-1, 'hours').unix(), ip: '178.155.4.168', userPublicId: 'nadine@afterlogic.com'},
+			{time: moment().add(-3, 'hours').unix(), ip: '178.155.4.168', userPublicId: 'nadine@afterlogic.com'},
+			{time: moment().add(-4, 'hours').unix(), ip: '178.155.4.123', userPublicId: ''},
+			{time: moment().add(-5, 'hours').unix(), ip: '178.155.4.168', userPublicId: 'nadine@afterlogic.com'},
+			{time: moment().add(-1, 'days').unix(), ip: '178.155.4.156', userPublicId: ''},
+			{time: moment().add(-2, 'days').unix(), ip: '178.155.4.168', userPublicId: 'nadine@afterlogic.com'},
+		],
+		Count: 27
+	};
+	// oResponse.Result = {
+	// 	Items: [],
+	// 	Count: 0
+	// };
+	if (oResponse.Result)
+	{
+		this.historyItems(oResponse.Result.Items);
+		this.oPageSwitcher.setCount(oResponse.Result.Count);
+	}
+	else
+	{
+		Screens.showError(TextUtils.i18n('%MODULENAME%/ERROR_GET_HISTORY'));
+	}
+	this.isLoading(false);
+};
+
+CShowHistoryPopup.prototype.confirmClearHistory = function ()
+{
+	Popups.showPopup(ConfirmPopup, [TextUtils.i18n('%MODULENAME%/CONFIRM_CLEAR_HISTORY'), _.bind(function (bClearHistory) {
+		if (bClearHistory)
+		{
+			this.clearHistory();
+		}
+	}, this), '', TextUtils.i18n('%MODULENAME%/ACTION_CLEAR_HISTORY')]);
+};
+
+CShowHistoryPopup.prototype.clearHistory = function ()
+{
+	Ajax.send('ActivityHistory',
+		'ClearActivityHistory',
+		{
+			'Type': this.item.storageType(),
+			'Path': this.item.path(),
+			'Name': this.item.fileName(),
+		},
+		this.onClearActivityHistory,
+		this
+	);
+};
+
+CShowHistoryPopup.prototype.onClearActivityHistory = function (oResponse, oRequest)
+{
+	oResponse.Result = true;
+	if (oResponse.Result)
+	{
+		this.historyItems([]);
+		this.oPageSwitcher.setCount(0);
+		Screens.showReport(TextUtils.i18n('%MODULENAME%/REPORT_CLEAR_HISTORY'));
+	}
+	else
+	{
+		Screens.showError(TextUtils.i18n('%MODULENAME%/ERROR_CLEAR_HISTORY'));
+	}
+	this.isLoading(false);
+};
+
+module.exports = new CShowHistoryPopup();
