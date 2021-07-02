b8efb90b37c5e17d9800b1ffddd7f38c3f218dee
sash04ek <a.ovcharov@gmail.com>
2021-07-02 10:50:56 +0300

    fixed issue with migration

--- Migrations/2021_06_29_123441_create_activity_history_table.php
+++ Migrations/2021_06_29_123441_create_activity_history_table.php
@@ -13,18 +13,29 @@ class CreateActivityHistoryTable extends Migration
      */
     public function up()
     {
-        Capsule::schema()->create('activity_history', function (Blueprint $table) {
-            $table->id('Id');
-            $table->integer('UserId')->default(0);
-            $table->string('ResourceType')->default('');
-            $table->string('ResourceId')->default('');
-            $table->string('IpAddress')->default();
-            $table->string('Action')->default('');
-            $table->integer('Timestamp')->default(0);
-            $table->string('GuestPublicId')->default('');
+        if (!Capsule::schema()->hasTable('activity_history')) {
+            Capsule::schema()->create('activity_history', function (Blueprint $table) {
+                $table->id('Id');
+                $table->integer('UserId')->default(0);
+                $table->string('ResourceType')->default('');
+                $table->string('ResourceId')->default('');
+                $table->string('IpAddress')->default();
+                $table->string('Action')->default('');
+                $table->integer('Timestamp')->default(0);
+                $table->string('GuestPublicId')->default('');
 
-            $table->timestamps();
-        });
+                $table->timestamps();
+            });
+        } else if (Capsule::schema()->hasColumn('activity_history', 'user_id')) {
+            Capsule::schema()->rename('id', 'Id');
+            Capsule::schema()->rename('user_id', 'UserId');
+            Capsule::schema()->rename('resource_type', 'ResourceType');
+            Capsule::schema()->rename('resource_id', 'ResourceId');
+            Capsule::schema()->rename('ip_address', 'IpAddress');
+            Capsule::schema()->rename('action', 'Action');
+            Capsule::schema()->rename('timestamp', 'Timestamp');
+            Capsule::schema()->rename('guest_public_id', 'GuestPublicId');
+        }
     }
 
     /**
